260a459d72d5eacf48e148e51bd72d318a55c3a6
Hugo Zeminian <hugozamian@hotmail.com>
2023-04-14 13:58:17 -0600

    perfomed get data from server on booking page

--- server_side/router/main_router.js
+++ server_side/router/main_router.js
@@ -1,179 +1,180 @@
 //To initialize router
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(express.json());
-const path = require('path');
-const fs = require('fs');
-const userService = require('../services/userService');
-const multer = require('multer'); // Multer is a middleware for handling multipart/form-data
-const { generateID } = require('../public');
+const path = require("path");
+const fs = require("fs");
+const userService = require("../services/userService");
+const multer = require("multer"); // Multer is a middleware for handling multipart/form-data
+const { generateID } = require("../public");
 
-router.use(express.static('./public')); //  to use script in html, need to use static inside router file instead server file. 
-router.use(express.static('./repository'))
+router.use(express.static("./public")); //  to use script in html, need to use static inside router file instead server file.
+router.use(express.static("./repository"));
 
-
-
-//Get the index.html page
-router.get('/', function (req, res) {
-    const filePath = path.join(__dirname, '../public/index.html');
-    res.sendFile(filePath);
+router.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
+//Get the index.html page
+router.get("/", function (req, res) {
+  const filePath = path.join(__dirname, "../public/index.html");
+  res.sendFile(filePath);
+});
 
 // Register new USER
-router.post('/newUser', (req, res) => {
-
-    userService.addUser(req.body)
-        .then(userFullName => {
-            res.status(200).send(userFullName);
-        })
-        .catch(err => {
-            res.status(err.code).send(err);
-        });
+router.post("/newUser", (req, res) => {
+  userService
+    .addUser(req.body)
+    .then((userFullName) => {
+      res.status(200).send(userFullName);
+    })
+    .catch((err) => {
+      res.status(err.code).send(err);
+    });
 });
 
 // to login
-router.post('/login', (req, res) => {
-
-    userService.login(req.body)
-        .then(userMatch => {
-            res.status(200).send(userMatch);
-        })
-        .catch(err => {
-            res.status(err.code).send(err);
-        });
+router.post("/login", (req, res) => {
+  userService
+    .login(req.body)
+    .then((userMatch) => {
+      res.status(200).send(userMatch);
+    })
+    .catch((err) => {
+      res.status(err.code).send(err);
+    });
 });
 
 // Register new Property
-router.post('/newProperty', (req, res) => {
-
-    userService.addProperty(req.body)
-        .then(newPropertyAddress => {
-            res.status(200).send(newPropertyAddress);
-        })
-        .catch(err => {
-            res.status(err.code).send(err);
-        });
+router.post("/newProperty", (req, res) => {
+  userService
+    .addProperty(req.body)
+    .then((newPropertyAddress) => {
+      res.status(200).send(newPropertyAddress);
+    })
+    .catch((err) => {
+      res.status(err.code).send(err);
+    });
 });
 
 // Register new Workspace
-router.post('/newWorkspace', (req, res) => {
-    userService.addWorkspace(req.body)
-        .then(newWorkspaceType => {
-            res.status(200).send(newWorkspaceType);
-        })
-        .catch(err => {
-            //  console.error('passed here', err);
-            res.status(err.code).send(err);
-        });
+router.post("/newWorkspace", (req, res) => {
+  userService
+    .addWorkspace(req.body)
+    .then((newWorkspaceType) => {
+      res.status(200).send(newWorkspaceType);
+    })
+    .catch((err) => {
+      //  console.error('passed here', err);
+      res.status(err.code).send(err);
+    });
 });
 
-router.get('/findPropertyByOwner', async (req, res) => {
+router.get("/findPropertyByOwner", async (req, res) => {
+  const user_id = req.query.user_id;
 
-    const user_id = req.query.user_id;
-
-    userService.findPropertyByOwner(user_id)
-        .then(filteredProperties => {
-            res.status(200).send(filteredProperties);
-        })
-        .catch(err => {
-            res.status(err.code).send(err);
-        });
+  userService
+    .findPropertyByOwner(user_id)
+    .then((filteredProperties) => {
+      res.status(200).send(filteredProperties);
+    })
+    .catch((err) => {
+      res.status(err.code).send(err);
+    });
 });
 
-router.get('/findWorkspaceByOwner', async (req, res) => {
+router.get("/findWorkspaceByOwner", async (req, res) => {
+  const user_id = req.query.user_id;
 
-    const user_id = req.query.user_id;
-
-    userService.findWorkspaceByOwner(user_id)
-        .then(filteredWorkspaces => {
-            res.status(200).send(filteredWorkspaces);
-        })
-        .catch(err => {
-            res.status(err.code).send(err);
-        });
+  userService
+    .findWorkspaceByOwner(user_id)
+    .then((filteredWorkspaces) => {
+      res.status(200).send(filteredWorkspaces);
+    })
+    .catch((err) => {
+      res.status(err.code).send(err);
+    });
 });
 
-router.get('/getWorkspaceByOwner', async (req, res) => {
+router.get("/getWorkspaceByOwner", async (req, res) => {
+  const user_id = req.query.user_id;
 
-    const user_id = req.query.user_id;
-
-    userService.getWorkspaceByOwner(user_id)
-        .then(filteredWorkspaces => {
-            res.status(200).send(filteredWorkspaces);
-        })
-        .catch(err => {
-            res.status(err.code).send(err);
-        });
+  userService
+    .getWorkspaceByOwner(user_id)
+    .then((filteredWorkspaces) => {
+      res.status(200).send(filteredWorkspaces);
+    })
+    .catch((err) => {
+      res.status(err.code).send(err);
+    });
 });
 
-router.get('/getReservedDate', async (req, res) => {
+router.get("/getReservedDate", async (req, res) => {
+  const workspace_id = req.query.workspace_id;
 
-    const workspace_id = req.query.workspace_id;
-
-    userService.getReservedDate(workspace_id)
-        .then(data => {
-            res.status(200).send(data);
-        })
-        .catch(err => {
-            res.status(err.code).send(err);
-        });
+  userService
+    .getReservedDate(workspace_id)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(err.code).send(err);
+    });
 });
 
+router.post("/delistWorkspace", async (req, res) => {
+  const user_id = req.query.user_id;
+  const workspace_id = req.query.workspace_id;
 
-
-router.post('/delistWorkspace', async (req, res) => {
-
-    const user_id = req.query.user_id;
-    const workspace_id = req.query.workspace_id;
-
-    userService.delistWorkspace(user_id, workspace_id)
-        .then(workpaceDelist => {
-            res.status(200).send(workpaceDelist);
-        })
-        .catch(err => {
-            res.status(err.code).send(err);
-        });
+  userService
+    .delistWorkspace(user_id, workspace_id)
+    .then((workpaceDelist) => {
+      res.status(200).send(workpaceDelist);
+    })
+    .catch((err) => {
+      res.status(err.code).send(err);
+    });
 });
 
-router.post('/delistProperty', async (req, res) => {
+router.post("/delistProperty", async (req, res) => {
+  const user_id = req.query.user_id;
+  const property_id = req.query.property_id;
 
-    const user_id = req.query.user_id;
-    const property_id = req.query.property_id;
-
-    userService.delistProperty(user_id, property_id)
-        .then(propertydelist => {
-            res.status(200).send(propertydelist);
-        })
-        .catch(err => {
-            res.status(err.code).send(err);
-        });
-})
-
+  userService
+    .delistProperty(user_id, property_id)
+    .then((propertydelist) => {
+      res.status(200).send(propertydelist);
+    })
+    .catch((err) => {
+      res.status(err.code).send(err);
+    });
+});
 
 //==================================Testing===========================//
 
-
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './repository/img'); // Set the destination folder for uploaded files
-    },
-    filename: function (req, file, cb) {
-        const workspaceId = 'a42';
-        const fileExtension = file.originalname.split('.').pop();
-        const fileName = `${workspaceId}${generateID()}.${fileExtension}`; // Set the file name for uploaded files
-        cb(null, fileName);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "./repository/img"); // Set the destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    const workspaceId = "a42";
+    const fileExtension = file.originalname.split(".").pop();
+    const fileName = `${workspaceId}${generateID()}.${fileExtension}`; // Set the file name for uploaded files
+    cb(null, fileName);
+  },
 });
 
 const upload = multer({ storage: storage });
 
-
-router.post('/upload', upload.single('workspaceImage'), function (req, res) {
-    res.send('File uploaded successfully'); // Return a response to the client indicating that the file was uploaded successfully
+router.post("/upload", upload.single("workspaceImage"), function (req, res) {
+  res.send("File uploaded successfully"); // Return a response to the client indicating that the file was uploaded successfully
 });
 
 //========================================================================//
